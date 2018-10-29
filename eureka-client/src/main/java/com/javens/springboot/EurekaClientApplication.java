930572b69516c920e39fe57a8c243e9d0829cd0d
liujing <liuj@dinglc.com>
2018-10-29 08:45:28 +0800

    eureka

--- eureka-client/src/main/java/com/javens/springboot/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/javens/springboot/EurekaClientApplication.java
@@ -2,14 +2,22 @@ package com.javens.springboot;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -18,29 +26,28 @@ import java.util.Set;
  */
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @ComponentScan("com.javens.springboot.*")
 @RestController
 public class EurekaClientApplication {
     @Autowired
-    private EurekaClient eurekaClient;//服务发现客户端
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    private RestTemplate restTemplate;
 
-
-    /*@GetMapping(value = "/add")
-    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = eurekaClient.getInstances("STORES").get(0);
-        Integer r = a + b;
-        System.out.println("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
-        return r;
-    }*/
-
-    @RequestMapping("/serviceUrl")
+    @RequestMapping("/hello")
     public String serviceUrl(){
-        Set set = eurekaClient.getAllKnownRegions();
-        System.out.println(set);
-        InstanceInfo instance = eurekaClient.getNextServerFromEureka("STORES", false);
-        return instance.getHomePageUrl();
+        discoveryClient.getServices().stream().forEach(x->{
+            System.out.println(x);
+        });
+        return "Hello World";
     }
+    @RequestMapping("/ribbon-consumer")
+    public String ribbonConsumer(){
+        return restTemplate.getForEntity("http://EUREKA-CLIENT/hello",
+                String.class) . getBody();
+    }
+
 
     @RequestMapping("/")
     public String home() {
@@ -49,5 +56,12 @@ public class EurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
